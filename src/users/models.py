f07aa004ece4991399be68d9ee64c17ad48992aa
Carlos Flores <carlosflores@Carloss-MacBook-Pro.local>
2020-07-08 17:30:20 -0400

    Updated user and api apps.

--- src/users/models.py
+++ src/users/models.py
@@ -25,10 +25,11 @@ class UserManager(BaseUserManager):
         return user
     
     def create_superuser(self, username, email, first_name, last_name, address, zip_code, city, country, password):
+
         user = self.create_user(
             username = username,
             password = password,
-            email = self.normalize_email(email),
+            email = email,
             first_name = first_name,
             last_name = last_name,
             address = address,
@@ -41,25 +42,25 @@ class UserManager(BaseUserManager):
         user.is_staff = True
         user.is_superuser = True
 
-        user.save(using = self._db)
+        user.save(using=self._db)
         return user
 
 class User(AbstractBaseUser):
     
-    username    = models.CharField(max_length = 30, unique = True)
-    email       = models.EmailField(verbose_name = 'email', max_length = 60, blank = True, default = None, unique = True, null=True)
-    first_name  = models.CharField(max_length = 30, blank = True, null=True)
-    last_name   = models.CharField(max_length = 30, blank = True, null=True)
-    address     = models.CharField("Address line 1", max_length = 1024, blank = True, null=True)
-    zip_code    = models.CharField("Zip/Postal Code", max_length = 5, blank = True, null=True)
-    city        = models.CharField("City", max_length = 1024, blank = True, null=True)
-    country     = models.CharField("Country", max_length = 100, blank = True, null=True)
-    date_joined = models.DateTimeField(verbose_name = 'date joined', auto_now_add = True)
-    last_login  = models.DateTimeField(verbose_name = 'last login', auto_now = True)
-    is_admin    = models.BooleanField(default = False)
-    is_active   = models.BooleanField(default = True)
-    is_staff    = models.BooleanField(default = False)
-    is_superuser= models.BooleanField(default = False)
+    username    = models.CharField(max_length=30, unique=True)
+    email       = models.EmailField(verbose_name='email', max_length=60, blank=True, default=None, unique=True, null=True)
+    first_name  = models.CharField(max_length=30, blank=True, null=True)
+    last_name   = models.CharField(max_length=30, blank=True, null=True)
+    address     = models.CharField("Address line 1", max_length=1024, blank=True, null=True)
+    zip_code    = models.CharField("Zip/Postal Code", max_length=5, blank=True, null=True)
+    city        = models.CharField("City", max_length=1024, blank=True, null=True)
+    country     = models.CharField("Country", max_length=100, blank=True, null=True)
+    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login  = models.DateTimeField(verbose_name='last login', auto_now=True)
+    is_admin    = models.BooleanField(default=False)
+    is_active   = models.BooleanField(default=True)
+    is_staff    = models.BooleanField(default=False)
+    is_superuser= models.BooleanField(default=False)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email', 'first_name', 'last_name', 'address', 'zip_code', 'city', 'country']
@@ -75,10 +76,15 @@ class User(AbstractBaseUser):
     def has_module_perms(self, app_label):
         return True
 
+    def save(self, *args, **kwargs):
+        if self.email == '':
+            self.email = None
+        models.Model.save(self, *args, **kwargs)
+
 class CreditCard(models.Model):
-    
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
     creditcard_number = models.CharField(max_length = 16)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
 
     def __str__(self):
-        return self.user + ', ' + self.creditcard_number
+        return self.creditcard_number
