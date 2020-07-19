2b8b81efeb62ca5a99c90b5c08fa66cf815ec614
Carlos Flores <carlosflores@Carloss-MacBook-Pro.local>
2020-07-19 15:18:41 -0400

    Sprint 2 ready.

--- src/cart/models.py
+++ src/cart/models.py
@@ -27,4 +27,4 @@ class ShoppingCartItems(models.Model):
     quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.cart.user.username + " - " + self.item_id
+        return self.item_id.title
